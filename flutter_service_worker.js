0bd2ef95bc29d97bcd410a3dca2bcf0477f6e156
Philippe Fanaro <philippefanaro@gmail.com>
2022-04-06 22:21:14 -0300

    v1.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,6 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"assets/assets/github.svg": "4b5a771a0e4316d7fe4fa2d5679dccaa",
+"assets/assets/twitch.svg": "d068546868bfdc741e910454c3ce7b0f",
+"assets/assets/youtube.svg": "35c645afb2fe2ec554466c125b04b718",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/NOTICES": "72e30942cc4340dd7bec2f7712386936",
@@ -11,7 +14,7 @@ const RESOURCES = {
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"favicon.png": "cc99fdd12c4b6ec6a16e257717feab95",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
